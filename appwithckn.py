4666b2bbdb01f524caccb058ef2f51164d83a000
phanttuan <tuanphan19405@gmail.com>
2024-11-25 22:51:21 +0700

    v3

--- appwithckn.py
+++ appwithckn.py
@@ -3,19 +3,20 @@ from tkinter import messagebox
 import requests
 from PIL import Image, ImageTk
 from customtkinter import CTkImage
-import io
 from datetime import datetime
-
 from pygments.styles.dracula import background
-
 api_key = '30d4741c779ba94c470ca1f63045390a'
 # api_key = '5dc4c93a4b357abf361daa99a99cd63b'
 # Lấy địa chỉ IP công cộng
 public_ip = requests.get("https://api64.ipify.org?format=json").json()["ip"]
 # Tra cứu vị trí
 response = requests.get(f"http://ip-api.com/json/{public_ip}").json()
+#Lấy vị trí cụ thể
 country = response.get("country", "Unknown")
 city = response.get("city", "Unknown")
+
+size_status_icon = (20, 20)
+#Đồng hồ
 class Clock:
     def __init__(self, root):
         self.root = root
@@ -37,60 +38,70 @@ class Clock:
 
 class WeatherAtLocation:
     def __init__(self):
-        ctk.set_appearance_mode("Dark")  # Modes: "System" (default), "Light", "Dark"
-        ctk.set_default_color_theme("blue")  # Themes: "blue" (default), "green", "dark-blue"
 
+        #Chọn theme của app
+        ctk.set_appearance_mode("dark")
+        ctk.set_default_color_theme("dark-blue")
+
+        #Thiết lập thông số màn hình chính
         self.root = ctk.CTk()
         self.root.geometry("1280x720+150+50")
         self.root.title("Application")
+        #Clock
+        self.clock = Clock(self.root)
 
+        #Tạo frame đầu tiên chứa thông tin thời tiết tại vị trí
         self.frame = ctk.CTkFrame(master=self.root, width=300, height=500, corner_radius=15, fg_color= "transparent")
         self.frame.place(x = 960, y = 20)
 
-        # Label Context
+        # Label của frame
         self.label = ctk.CTkLabel(master = self.frame, text="Weather", font=('Arial', 18), text_color= '#B3B3B3')
         self.label.pack(pady = 10)
-        #Show weather
+
+        #Hiển thị vị trí tại địa chỉ IP
         self.show_weather()
-        #Clock
-        self.clock = Clock(self.root)
-        #Framne 2
+
+        #Frame 2 chứa lựa chọn truy cập đếp vị trí khác hoặc dự báo thời tiết
         self.create_frame2()
         self.root.mainloop()
+    #Hàm tạo ra Frame 2   
     def create_frame2(self):
         self.frame2 = ctk.CTkFrame(master = self.root, width= 300, height= 80, corner_radius= 15, fg_color= "transparent")
         self.frame2.place(x = 950,y = 460)
 
-
         self.label = ctk.CTkLabel(master=self.frame2, text="Vị trí khác", font=('Arial', 18),
                                   text_color='#B3B3B3')
         self.label.pack(pady=2)
+
         self.textbox = ctk.CTkTextbox(master = self.frame2, width= 255, height= 1, corner_radius= 15, fg_color= "#2A2A2A",
                                         text_color= "#B2B2B2", font = ("Arial", 18), border_width= 2, border_color= "grey",
                                       activate_scrollbars= False )
         self.textbox.bind("<Return>", self.shortcut)
         self.textbox.pack(pady = 5)
         self.location = self.textbox.get("1.0", ctk.END).strip()
+
         self.button = ctk.CTkButton(master = self.frame2, width= 120, height= 20, corner_radius= 15,
                                     hover_color= "#4158D0", text = "OK", font = ("Arial", 18), border_width= 2, border_color= "#B2B2B2",
                                     command = self.show_weather_location)
         self.button.pack(pady = 8)
 
-        # Label Context
+        # Label nội dung
         self.label = ctk.CTkLabel(master=self.frame2, text="Weather Forecast", font=('Arial', 18), text_color='#B3B3B3')
         self.label.pack(pady=11)
-        #Button forcast
+        #Nút dự báo thời tiết
         self.button2_presses = False
         self.button2 = ctk.CTkButton(master = self.frame2, width= 120, height= 20, corner_radius= 15,
                                     hover_color= "#4158D0", text = "FORECAST", font = ("Arial", 18), border_width= 2, 
                                     border_color= "#B2B2B2",
                                     command = self.handle_button2_click)
         self.button2.pack(pady = 14)
+    #Hàm xử lí sự kiện khi nhấn nút "OK" để hiển thị thời tiết ở vị trí khác
     def handle_button2_click(self):
         self.button2_presses = not self.button2_presses
         if self.button2_presses:
             self.show_weather_forecast(city)
             self.new_frame2()
+    #Hàm hiển thị thời tiết tại vị trí IP
     def show_weather(self):
         location = city
         try:
@@ -99,7 +110,9 @@ class WeatherAtLocation:
             ).json()
             if weather_data['cod'] == 404:
                 messagebox.showerror("Error", "No City Found")
+            
             else:
+                # Xử lí dữ liệu
                 weather = weather_data['weather'][0]['main']
                 temp = round(weather_data['main']['temp'])
                 tempC = round(((temp - 32) * 5) / 9)
@@ -109,36 +122,26 @@ class WeatherAtLocation:
                 tempmaxC = round(((tempmax - 32) * 5) / 9)
                 humidity = weather_data['main']['humidity']
                 visibility = weather_data['visibility']
-                size_image = (200, 200)
-                icon_path = ""
-                if 'clear' in weather.lower():
-                    icon_path = "Icon/clear.png"
-                if 'cloud' in weather.lower():
-                    icon_path = "Icon/cloud.png"
-                if 'rain' in weather.lower():
-                    icon_path = "Icon/rain.png"
-                if 'snow' in weather.lower():
-                    icon_path = "Icon/snow.png"
-                if 'storm' in weather.lower():
-                    icon_path = "Icon/storm.png"
-
-                image = Image.open(icon_path)
-                    # Label chứa icon
-                picture = ctk.CTkImage(dark_image=image, light_image= image, size = size_image)
-                label_picture = ctk.CTkLabel(master = self.frame, image = picture, text = "")
-                label_picture.pack()
-                label_text = ctk.CTkLabel(
-                        master = self.frame,
-                        text=f"Vị Trí: {location}\nThời tiết: {weather}\nNhiệt độ: {tempC}°C\n"
-                             f"Thấp nhất: {tempminC}°C, Cao nhất: {tempmaxC}°C\nĐộ ẩm: {humidity}\nTầm nhìn xa: {visibility}",
-                        font=('Arial', 18),
-                    )
-                label_text.pack(pady = 30)
+                self.show_weather_icon(weather, size_image = (150, 150))
+                # Tạo các Label với icon
+                self.load_icon("Icon/location.png", text = f"{location}")
+                self.load_icon("Icon/temperature.png", text = f"{tempC}°C")
+                self.load_icon("Icon/temperaturerange.png", text = f"{tempmaxC}°C / {tempminC}°C")
+                self.load_icon("Icon/weather.png", text = f"{weather}")
+                self.load_icon("Icon/humidity.png", text=f"{humidity}")
+                self.load_icon("Icon/visibility.png", text=f"{visibility}")
         except Exception as e:
             messagebox.showerror("Error", f"Lỗi mạng, không truy cập được vị trí")
 
+    def load_icon(self, icon_path, size = size_status_icon, text = ""):
+        icon = Image.open(icon_path)
+        icon_picture = ctk.CTkImage(dark_image=icon, light_image=icon, size=size)
+        icon_label = ctk.CTkLabel(master=self.frame, text=f":\t{text}", image=icon_picture, compound= "left", font=('Arial', 18))
+        icon_label.pack(pady = 5, anchor = "w")
+
+    #Hàm hiển thị thời tiết tại vị trí chỉ định trong textbox
     def show_weather_location(self):
-        # Get the location from the textbox
+        # Lấy vị trí từ Textbox
         location = self.textbox.get("1.0", ctk.END).strip()
 
         try:
@@ -149,11 +152,11 @@ class WeatherAtLocation:
             if weather_data['cod'] == 404:
                 messagebox.showerror("Error", "No City Found in frame 1")
             else:
-                # Clear existing widgets in frame1
+                # Xóa frame 1
                 for widget in self.frame.winfo_children():
                     widget.destroy()
 
-                # Extract weather information
+                # Xử lí dữ liệu
                 weather = weather_data['weather'][0]['main']
                 temp = round(weather_data['main']['temp'])
                 tempC = round(((temp - 32) * 5) / 9)
@@ -163,45 +166,49 @@ class WeatherAtLocation:
                 tempmaxC = round(((tempmax - 32) * 5) / 9)
                 humidity = weather_data['main']['humidity']
                 visibility = weather_data['visibility']
-                size_image = (200, 200)
-                icon_path = ""
-                # Determine the appropriate icon based on weather conditions
-                if 'clear' in weather.lower():
-                    icon_path = "Icon/clear.png"
-                elif 'cloud' in weather.lower():
-                    icon_path = "Icon/cloud.png"
-                elif 'rain' in weather.lower():
-                    icon_path = "Icon/rain.png"
-                elif 'snow' in weather.lower():
-                    icon_path = "Icon/snow.png"
-                elif 'storm' in weather.lower():
-                    icon_path = "Icon/storm.png"
 
-                # Load the icon image
-                image = Image.open(icon_path)
-                picture = ctk.CTkImage(dark_image=image, light_image=image, size=size_image)
+                #Hiển thị icon thời tiết
+                self.show_weather_icon(weather, size_image = (150, 150))
 
-                # Create and display the new weather information
-                label_picture = ctk.CTkLabel(master=self.frame, image=picture, text="")
-                label_picture.pack()
-
-                label_text = ctk.CTkLabel(
-                    master=self.frame,
-                    text=f"Vị Trí: {location}\nThời tiết: {weather}\nNhiệt độ: {tempC}°C\n"
-                         f"Thấp nhất: {tempminC}°C, Cao nhất: {tempmaxC}°C\nĐộ ẩm: {humidity}\nTầm nhìn xa: {visibility}",
-                    font=('Arial', 18),
-                )
-                label_text.pack(pady=30)
+                # Tạo các Label với icon
+                self.load_icon("Icon/location.png", text = f"{location}")
+                self.load_icon("Icon/temperature.png", text = f"{tempC}°C")
+                self.load_icon("Icon/temperaturerange.png", text = f"{tempmaxC}°C / {tempminC}°C")
+                self.load_icon("Icon/weather.png", text = f"{weather}")
+                self.load_icon("Icon/humidity.png", text=f"{humidity}")
+                self.load_icon("Icon/visibility.png", text=f"{visibility}")
                 self.clear_text_box()
 
         except Exception as e:
             messagebox.showerror("Error", f"Nhập sai tên, vui lòng nhập lại: {e}")
 
+    def show_weather_icon(self, weather, size_image):
+        icon_path = ""
+        # Hiển thị icon dựa trên thông tin trả về
+        if 'clear' in weather.lower():
+            icon_path = "Icon/clear.png"
+        elif 'cloud' in weather.lower():
+            icon_path = "Icon/cloud.png"
+        elif 'rain' in weather.lower():
+            icon_path = "Icon/rain.png"
+        elif 'snow' in weather.lower():
+            icon_path = "Icon/snow.png"
+        elif 'storm' in weather.lower():
+            icon_path = "Icon/storm.png"
+
+        # Load và hiển thị ảnh icon tình hình thời tiết
+        image = Image.open(icon_path)
+        picture = ctk.CTkImage(dark_image=image, light_image=image, size=size_image)
+        label_picture = ctk.CTkLabel(master=self.frame, image=picture, text="")
+        label_picture.pack()
+    #Hàm xử lí nút "Enter" khi nhập
     def shortcut(self, event):
         if event.keysym == "Return":
             self.show_weather_location()
+    #Hàm xóa nội dung trong Textbox
     def clear_text_box(self ):
         self.textbox.delete("0.0", "end")
+    #Hàm dự báo thời tiết tại khu vực nhập
     def show_weather_forecast(self, location):
         try:
             # Gửi yêu cầu API để lấy thông tin dự báo thời tiết
@@ -283,23 +290,13 @@ class WeatherAtLocation:
                 row += 1
 
             # Nút Back để quay lại giao diện thời tiết chính
-            back_button = ctk.CTkButton(
-                master=self.frame,
-                width=120,
-                height=20,
-                corner_radius=15,
-                hover_color="#4158D0",
-                text="Back",
-                font=("Arial", 18),
-                border_width=2,
-                border_color="#B2B2B2",
-                command=self.back_function
+            back_button = ctk.CTkButton(master=self.frame, width=120, height=20, corner_radius=15, hover_color="#4158D0", text="Back", 
+                font=("Arial", 18),border_width=2, border_color="#B2B2B2", command=self.back_function
             )
             back_button.grid(column=1, row=row, padx=5, pady=10, columnspan=2)
 
         except Exception as e:
             messagebox.showerror("Error", f"Lỗi mạng hoặc dữ liệu không khả dụng: {e}")
-
 
     def back_function(self):
         for widget in self.frame.winfo_children():
